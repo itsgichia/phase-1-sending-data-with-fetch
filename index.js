38136f58a2a3b330117b3dafb30fb546013a65b1
itsgichia <gichiamuiruri@gmail.com>
2024-01-08 21:47:35 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(){
+    const username = 'Gichia';
+    const userEmail = 'gichiamuiruri@icloud.com'
+    
+    console.log(username);
+    console.log(userEmail);
+
+
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+    },
+    body: JSON.stringify({
+        username: "user",
+        userEmail:"gichiamuiruri@icloud.com",
+    })
+};
+
+fetch("http://localhost:3000/users",configurationObject)
+    .then(function (response){
+        return response.json();
+})
+    .then(function(object){
+        console.log(object);
+})
+    .catch(function(error){
+        console.log(error.message);
+});
+return fetch("http://localhost:3000/users", configurationObject);
+}
+submitData();
